485a66ad4b5156e732245e521ede40fc3de7d944
navdeep1840 <navikitu001@gmail.com>
2022-06-27 20:49:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const btn = document.getElementById("menu-btn");
+const nav = document.getElementById("menu");
+
+function navToggle() {
+  btn.classList.toggle("open");
+  nav.classList.toggle("hidden");
+  document.body.classList.toggle("no-scroll");
+}
+
+btn.addEventListener("click", navToggle);
+
+// slider
+
+var swiper = new Swiper(".slide-content", {
+  spaceBetween: 20,
+  loop: true,
+
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 1,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
